a34fe7834fff1c6e980279e704d9d88ce487c04e
Eric Sun <ericsun977@gmail.com>
2015-08-28 16:41:48 -0400

    rebuild

--- dist/ng-swiftype.js
+++ dist/ng-swiftype.js
@@ -1,6 +1,109 @@
 angular.module('ngSwiftype',[]);
 angular.module('ngSwiftype').
 
+directive('swiftypeAutocomplete', [ 'ngSwiftype.api', function(api){
+
+  var SwiftypeController = ['$scope', function( $scope ) {
+    
+
+    $scope.results = {},
+    $scope.error = {},
+    $scope.loading = false;
+
+    var params = {};
+
+    function result_handler(response) {
+
+      var info = response.data.info,
+          records = response.data.records,
+          total_records = 0;
+
+      for(key in info) {
+        $scope.results[key] = info[key];
+        $scope.results[key].records = records[key];
+
+        total_records += info[key].total_result_count;
+
+      };
+
+      $scope.results.record_count = total_records; // get total search result
+
+      $scope.loading = false; // finish loading
+
+    }
+
+    function query_from_swiftype(page, document_types) {
+
+      $scope.loading = true; // start loading
+
+      params.q = $scope.term;
+      params.engine_key = $scope.engine_key; // TODO: change this to CONST
+      params.per_page = $scope.limits;
+      
+      params.fetch_fields = $scope.fetch_fields;
+      params.search_fields = $scope.search_fields;
+      params.filters = $scope.filters;
+
+      // params.document_types = document_types || undefined; // TODO: swiftype reponse error when posting proper array type param, need to figure out why.
+      params.page = page;
+
+      api.search(params).then(result_handler);
+    }
+
+    $scope.search_by = function(page, document_type) {
+      query_from_swiftype(page, document_type);
+    };
+    
+    $scope.keyup = function(event) {
+
+      var keycode = event.keyCode || event.which;
+
+      // ignore arrow keys, shift
+      if (((keycode > 36) && (keycode < 41)) || (keycode == 16)) {
+        return false;
+      }
+      
+      if ( $scope.term && $scope.term.length > 2 ) {
+        query_from_swiftype($scope.page);
+      } else {
+        $scope.results = {};
+        $scope.$apply();
+      }
+    };
+
+  }];
+
+  return {
+    restrict: 'CE',
+    controller: SwiftypeController,
+    link: function(scope, element, attributes) {
+
+
+      if(!attributes.engineKey) {
+         scope.error = {
+          statusText : 'Missing engine key'
+        };
+        return false;
+      }
+
+      scope.engine_key = attributes.engineKey;
+      scope.limits = attributes.limits;
+      
+      if(attributes.fetchFields) {
+        scope.fetch_fields = JSON.parse(attributes.fetchFields);  
+      }
+
+      element.bind('keyup', scope.keyup);
+    }
+  };
+}]);
+angular.module('ngSwiftype').
+
+factory('ngSwiftype.cache', [ '$cacheFactory', function($cacheFactory){
+  return $cacheFactory('cache');
+}]);
+angular.module('ngSwiftype').
+
 
 service('ngSwiftype.api', [ '$http', 'ngSwiftype.cache', function($http, SwiftypeCache ) {
 
@@ -8,7 +111,11 @@ service('ngSwiftype.api', [ '$http', 'ngSwiftype.cache', function($http, Swiftyp
 
   this.search = function(params) {
 
-    var config = params || {};
+    if(!params && typeof params !== 'object') {
+      return false;
+    }
+
+    var config = {};
 
     config.q = params.q;
     config.engine_key = params.engine_key;
@@ -30,128 +137,3 @@ service('ngSwiftype.api', [ '$http', 'ngSwiftype.cache', function($http, Swiftyp
     });
   }
 }]);
-angular.module('ngSwiftype').
-
-factory('ngSwiftype.cache', [ '$cacheFactory', function($cacheFactory){
-  return $cacheFactory('cache');
-}]);
-angular.module('ngSwiftype').
-
-directive('swiftypeAutocomplete', [ 'ngSwiftype.api', function(api){
-
-  var SwiftypeController = ['$scope', function( $scope ) {
-    
-    $scope.results = {},
-    $scope.error = {},
-    $scope.loading = false;
-
-    var params = {};
-
-    function success_handler(response) {
-
-      var info = response.data.info,
-          records = response.data.records,
-          total_records = 0;
-
-      for(key in info) {
-        $scope.results[key] = info[key];
-        $scope.results[key].records = records[key];
-
-        total_records += info[key].total_result_count;
-
-      };
-
-      $scope.results.record_count = total_records; // get total search result
-
-      $scope.loading = false; // finish loading
-
-    }
-
-    function failed_handler(response) {
-      $scope.error = {
-        status: response.status,
-        statusText: response.statusText
-      };
-
-      $scope.loading = false; // finish loading
-    }
-
-    function query_from_swiftype(page, document_types) {
-
-      $scope.loading = true; // start loading
-
-      params.q = $scope.term;
-      params.engine_key = $scope.engine_key; // TODO: change this to CONST
-      params.per_page = $scope.limits;
-      
-      params.fetch_fields = $scope.fetch_fields;
-      params.search_fields = $scope.search_fields;
-      params.filters = $scope.filters;
-
-      // params.document_types = document_types || undefined; // TODO: swiftype reponse error when posting proper array type param, need to figure out why.
-      params.page = page || 1;
-
-      api.search(params).then(success_handler, failed_handler);
-    }
-
-    function deactive_swiftype() {
-      // setting a flag to rootscope for closing Swiftype
-      $rootScope.swiftype_active = false;
-    }
-
-    $scope.search_by = function(page, document_type) {
-      query_from_swiftype(page, document_type);
-    };
-    
-    $scope.keyup = function(event) {
-
-      var keycode = event.keyCode || event.which;
-
-      // ignore arrow keys, shift
-      if (((keycode > 36) && (keycode < 41)) || (keycode == 16)) {
-        return;
-      }
-      
-      if ( $scope.term && $scope.term.length > 2 ) {
-        query_from_swiftype($scope.page);
-      } else {
-        $scope.results = {};
-        $scope.$apply();
-      }
-    };
-
-  }];
-
-  return {
-    restrict: 'CE',
-    controller: SwiftypeController,
-    link: function(scope, element, attributes) {
-
-      scope.engine_key = attributes.engineKey;
-      scope.limits = attributes.limits || 20;
-      
-      if(attributes.fetchFields) {
-        scope.fetch_fields = JSON.parse(attributes.fetchFields);  
-      }
-      if(attributes.searchFields) {
-        scope.search_fields = JSON.parse(attributes.searchFields);  
-      }
-      if(attributes.filters) {
-        scope.filters = JSON.parse(attributes.filters);
-      }
-
-      element.bind('keyup', scope.keyup);
-    }
-  };
-}]);
-angular.module('ngSwiftype').
-
-directive('autoFocus', function() {
-  return {
-    link: {
-      post: function postLink(scope, element) {
-        element[0].focus();
-      }
-    }
-  }
-});
