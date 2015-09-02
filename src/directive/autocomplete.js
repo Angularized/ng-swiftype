1524c205253c61e0898a5bfbd364a4372687c4b7
Eric Sun <ericsun977@gmail.com>
2015-09-02 14:10:01 -0400

    jsFormat

--- src/directive/autocomplete.js
+++ src/directive/autocomplete.js
@@ -1,12 +1,11 @@
 angular.module('ngSwiftype').
 
-directive('swiftypeAutocomplete', [ 'ngSwiftype.api', function(api){
+directive('swiftypeAutocomplete', ['ngSwiftype.api', function(api) {
 
-  var SwiftypeController = ['$scope', function( $scope ) {
-    
+  var SwiftypeController = ['$scope', function($scope) {
 
-    $scope.results = {},
-    $scope.error = {},
+    $scope.results = {};
+    $scope.error = {};
     $scope.loading = false;
 
     var params = {};
@@ -14,16 +13,15 @@ directive('swiftypeAutocomplete', [ 'ngSwiftype.api', function(api){
     function result_handler(response) {
 
       var info = response.data.info,
-          records = response.data.records,
-          total_records = 0;
+        records = response.data.records,
+        total_records = 0;
 
-      for(key in info) {
+      for (var key in info) {
         $scope.results[key] = info[key];
         $scope.results[key].records = records[key];
 
         total_records += info[key].total_result_count;
-
-      };
+      }
 
       $scope.results.record_count = total_records; // get total search result
 
@@ -38,7 +36,7 @@ directive('swiftypeAutocomplete', [ 'ngSwiftype.api', function(api){
       params.q = $scope.term;
       params.engine_key = $scope.engine_key; // TODO: change this to CONST
       params.per_page = $scope.limits;
-      
+
       params.fetch_fields = $scope.fetch_fields;
       params.search_fields = $scope.search_fields;
       params.filters = $scope.filters;
@@ -52,7 +50,7 @@ directive('swiftypeAutocomplete', [ 'ngSwiftype.api', function(api){
     $scope.search_by = function(page, document_type) {
       query_from_swiftype(page, document_type);
     };
-    
+
     $scope.keyup = function(event) {
 
       var keycode = event.keyCode || event.which;
@@ -61,8 +59,8 @@ directive('swiftypeAutocomplete', [ 'ngSwiftype.api', function(api){
       if (((keycode > 36) && (keycode < 41)) || (keycode == 16)) {
         return false;
       }
-      
-      if ( $scope.term && $scope.term.length > 2 ) {
+
+      if ($scope.term && $scope.term.length > 2) {
         query_from_swiftype($scope.page);
       } else {
         $scope.results = {};
@@ -78,18 +76,18 @@ directive('swiftypeAutocomplete', [ 'ngSwiftype.api', function(api){
     link: function(scope, element, attributes) {
 
 
-      if(!attributes.engineKey) {
-         scope.error = {
-          statusText : 'Missing engine key'
+      if (!attributes.engineKey) {
+        scope.error = {
+          statusText: 'Missing engine key'
         };
         return false;
       }
 
       scope.engine_key = attributes.engineKey;
       scope.limits = attributes.limits;
-      
-      if(attributes.fetchFields) {
-        scope.fetch_fields = JSON.parse(attributes.fetchFields);  
+
+      if (attributes.fetchFields) {
+        scope.fetch_fields = JSON.parse(attributes.fetchFields);
       }
 
       element.bind('keyup', scope.keyup);
